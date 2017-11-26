0863d19d40c7861316b075eaaf6da80a717abd7a
rolandovillca <rolandovillca@gmail.com>
2017-11-25 21:01:45 -0500

    Adding example for callbacks

---
+++ callbacks/example01.js
@@ -0,0 +1,17 @@
+var myCallback = function(err, data) {
+    if (err) throw err; // Check for the error and throw if it exists.
+    console.log('got data: ', data); // Otherwise proceed as usual.
+};
+
+var usingItNow = function(callback){
+    callback(null, 'get it?') // I dont want to throw an error, so I pass null for the error argument
+}
+
+// If we want to simulate an error case, we can define usingItNow like this
+var usingItNow = function(callback) {
+    var myError = new Error('My custom error!')
+    callback(myError, 'get it?')
+};
+
+// The final usage is exactly the same as in above
+usingItNow(myCallback)
